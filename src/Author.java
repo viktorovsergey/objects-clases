b0886e2714ccb16d4c770a2100b4cb7afa67b3e0
ya_zabiyaka <ya_zabiyaka@bk.ru>
2023-03-06 14:24:52 +0300

    все переделал

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String family;
+    private String name;
+
+    public Author(String family, String name) {
+        this.family = family;
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
